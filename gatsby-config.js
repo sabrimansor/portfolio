46366b9a84556ecfe5a4b552be96226830ff69c8
Sabri Mansor <sabri_mansor@aiap.sg>
2023-07-14 15:48:09 +0800

    feat: first version portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,39 +2,98 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: "/portfolio",
   siteMetadata: {
-    title: `My Portfolio Website`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingIds: [
-          "G-6NEZ973FX3", // Google Analytics / GA
-        ],
-        pluginConfig: {
-          head: true,
-        },
+      title: `Portfolio`,
+      siteUrl: `https://sabrimansor.github.io/`,
+      description: "Portfolio Website",
+      author: "Sabri Mansor",
+      siteLanguage: "en",
+      image: "banner.jpg",
+      titleTemplate: "sabrimansor",
+      getform_url: "",
+      socials: [
+          {
+              id: 0,
+              title: "github",
+              path: "https://github.com/sabrimansor",
+              icon: "Github",
+          },
+          {
+              id: 1,
+              title: "facebook",
+              path: "https://www.facebook.com/saabri.mansor/",
+              icon: "Facebook",
+          },
+          {
+              id: 2,
+              title: "linkedin",
+              path: "https://www.linkedin.com/in/sabri-mansor/",
+              icon: "Linkedin",
+          },
+      ],
+      contact: {
+          phone: "",
+          email: "",
       },
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  },
+  plugins: [
+      {
+          resolve: "gatsby-plugin-sass",
+          options: {
+              useResolveUrlLoader: {
+                  options: {
+                      sourceMap: true, //default is false
+                  },
+              },
+          },
+      },
+      "gatsby-plugin-image",
+      "gatsby-plugin-react-helmet",
+      "gatsby-plugin-sitemap",
+      "gatsby-plugin-sharp",
+      "gatsby-transformer-sharp",
+      "gatsby-transformer-json",
+      {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+              name: `images`,
+              path: `${__dirname}/src/assets/images`,
+              ignore: [`**/\.*`],
+          },
+      },
+      {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+              name: `data`,
+              path: `${__dirname}/src/data`,
+              ignore: [`**/\.*`],
+          },
+      },
+      {
+          resolve: `gatsby-transformer-remark`,
+          options: {
+              plugins: [
+                  {
+                      resolve: `gatsby-remark-images`,
+                      options: {
+                          maxWidth: 1200,
+                      },
+                  },
+              ],
+          },
+      },
+      {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+              name: "Sabri Mansor",
+              short_name: "sabrimansor",
+              theme_color: "#ff014f",
+              background_color: "#ffffff",
+              display: "standalone",
+              scope: "/",
+              start_url: "/",
+              icon: "src/assets/images/favicon.jpg",
+          },
+      },
+  ],
 };
